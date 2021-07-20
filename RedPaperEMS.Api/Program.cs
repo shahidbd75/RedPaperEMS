e1cf71c5957bbf80bec182514b7917b3950e36e8
Md. Abdul Baset Shahid <shahidab@live.com>
2021-07-20 18:57:24 +0600

    feat: Add Logger service -Serilog

--- RedPaperEMS.Api/Program.cs
+++ RedPaperEMS.Api/Program.cs
@@ -1,11 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Serilog;
 
 namespace RedPaperEMS.Api
 {
@@ -13,7 +11,25 @@ namespace RedPaperEMS.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json").Build();
+
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(config)
+                .WriteTo.File("Logs/log-.txt", rollingInterval:RollingInterval.Day)
+                .CreateLogger();
+
+
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
